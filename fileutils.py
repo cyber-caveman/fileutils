f63d586102e6a0befeb233db08dbbdbcd25e2f6d
Elier Carcases <elicarcases7@gmail.com>
2022-11-16 16:13:08 -0500

    Initializing support for other file types

--- fileutils.py
+++ fileutils.py
@@ -1,3 +1,4 @@
+#fileutils v1.0.1
 import os
 
 def getconfig(config_file, extension:str = None, verbose:bool = False):
@@ -6,26 +7,26 @@ def getconfig(config_file, extension:str = None, verbose:bool = False):
   file = open(config_file, 'r') #open config file with variables
   contents = file.readlines()
   file.close()
+  if(extension == None):
+    line_num=1
+    for line in contents:
+      if not line.__contains__("="):
+        print(f"{config_file}: line{line_num} --> \
+          {line.strip(line_sep)}: not an assigned parameter. Expecting <PARAM>=<VALUE>")
+        continue
+      dict_key = line.split("=")[0].replace(" ", "")
 
-  line_num=1
-  for line in contents:
-    if not line.__contains__("="):
-      print(f"{config_file}: line{line_num} --> \
-        {line.strip(line_sep)}: not an assigned parameter. Expecting <PARAM>=<VALUE>")
-      continue
-    dict_key = line.split("=")[0].replace(" ", "")
+      if verbose == True: #verbose
+        print("Parameter:" + dict_key)
+
+      value = line.split("=")[1].replace(" ", "").strip(line_sep)
+
+      if verbose == True: #verbose
+        print("Value: " + str(value))
+
+      params[dict_key]=value
+      line_num += 1
 
     if verbose == True: #verbose
-      print("Parameter:" + dict_key)
-
-    value = line.split("=")[1].replace(" ", "").strip(line_sep)
-
-    if verbose == True: #verbose
-      print("Value: " + str(value))
-
-    params[dict_key]=value
-    line_num += 1
-
-  if verbose == True: #verbose
-    print(f'Parameters read:\n{params}') #DEBUG
+      print(f'Parameters read:\n{params}') #DEBUG
   return params
