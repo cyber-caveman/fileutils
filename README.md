65b4b52e62784b3f32077ed8cdf668a548d9fc41
Elier Carcases <elicarcases7@gmail.com>
2022-11-16 16:50:08 -0500

    v1.0.1

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Module to facilitate interacting with files, for example, config files for Python scripts.
 
+getconfig(config_file, verbose:bool = False)
+Returns a dictionary containing the parameters in a config file passed as reference. Values and keys in the dictionary are of type string.
+
+
--- fileutils.py
+++ fileutils.py
@@ -1,32 +1,36 @@
-#fileutils v1.0.0
+#fileutils v1.0.1
 import os
 
 def getconfig(config_file, verbose:bool = False):
+  #Returns a dictionary containing the parameters in a config file passed as reference. 
+  #Values and keys in the returned dictionary are of type string.
+
   params={}
   line_sep=os.linesep #retrieves the line separator for this OS
-  file = open(config_file, 'r') #open config file with variables
-  contents = file.readlines()
-  file.close()
+  try:
+    with open(config_file, 'r') as file:  #open config file with variables
+      contents = file.readlines()
+      file.close()
 
-  line_num=1
-  for line in contents:
-    if not line.__contains__("="):
-      print(f"{config_file}: line{line_num} --> \
-        {line.strip(line_sep)}: not an assigned parameter. Expecting <PARAM>=<VALUE>")
-      continue
-    dict_key = line.split("=")[0].replace(" ", "")
+      line_num=1
+      for line in contents:
+        if not line.__contains__("="):
+          print(f"{config_file}: line {line_num} --> \
+            {line.strip(line_sep)}: Parameter not assigned, or empty line. Expecting <PARAM>=<VALUE>.\
+              Ignoring, reading next line...")
+          continue
+        dict_key = line.split("=")[0].replace(" ", "")
 
-    if verbose == True: #verbose
-      print("Parameter:" + dict_key)
+        value = line.split("=")[1].replace(" ", "").strip(line_sep)
 
-    value = line.split("=")[1].replace(" ", "").strip(line_sep)
+        if verbose == True: #verbose
+          print(f"Parameter: {dict_key} , Value: {value}")
 
-    if verbose == True: #verbose
-      print("Value: " + str(value))
+        params[dict_key]=value
+        line_num += 1
 
-    params[dict_key]=value
-    line_num += 1
-
-  if verbose == True: #verbose
-    print(f'Parameters read:\n{params}') #DEBUG
+      if verbose == True: #verbose
+        print(f'Parameters read:\n{params}') #DEBUG
+  except FileNotFoundError:
+    print(f"ERROR!!: File {config_file} not found, make sure that the path is correct.")
   return params
